de91cd115a3225ea73e05ebfcf26dab8a5077448
Alex Crome <alex.crome@bbconsult.co.uk>
2019-12-26 01:35:02 +0300

    forked from html2pugPostprocessor just to have nodejs + TS project

---
+++ run.bat
@@ -0,0 +1,4 @@
+
+rem node build/src/main.js test/inputFiles/f1.html test/inputFiles/f1.pug
+
+node build/src/main.js "c:\work\bberry\ewsp\Hanafin4\src\BBWT.Web\ClientApp\app\components\site\run-details\run-details.component.html" "c:\work\bberry\ewsp\Hanafin4\src\BBWT.Web\ClientApp\app\components\site\run-details\run-details.component.pug"
